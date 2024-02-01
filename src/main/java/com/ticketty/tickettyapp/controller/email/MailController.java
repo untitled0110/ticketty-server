33d8fd0fa0bde691e650507f18f7354e7d570954
Ji Yook <azure6.dev@gmail.com>
2024-02-02 02:08:51 +0900

    chore: 예상하고 있는 예외 상황에 대해서는 status 코드를 400이 아닌 200을 리턴해주도록 수정 예상하는 예외 상황 - 인증코드 발송 시: 이메일 밸리데이션 에러, 이메일 중복
    - 인증코드 확인 + 회원가입 시:
    패스워드 밸리데이션 에러, 이메일 중복, 인증코드 만료, 인증 실패(입력한 인증코드가 메일로 전송된 코드와 다른 경우)

--- src/main/java/com/ticketty/tickettyapp/controller/email/MailController.java
+++ src/main/java/com/ticketty/tickettyapp/controller/email/MailController.java
@@ -25,47 +25,31 @@ public class MailController {
 
         if (!emailValidator.test(request.getEmail())) {
             MailCodeResponse validationErrorResponse = new MailCodeResponse("null", false, "EMAIL_VALIDATION");
-            return ResponseEntity.badRequest().body(validationErrorResponse);
+            return ResponseEntity.ok(validationErrorResponse);
         }
 
         MailCodeResponse response = mailService.sendMail(request);
 
-        if (response.isSuccess()) {
-            return ResponseEntity.ok(response);
-        } else {
-            return ResponseEntity.badRequest().body(response);
-        }
+        return ResponseEntity.ok(response);
     }
 
     @PostMapping("/user/signup")
     public ResponseEntity<MailVerifyResponse> verifyMailAndSignup(@RequestBody MailVerifyRequest request) {
 
-        // 인증 실패 시 바로 반환
-        MailVerifyResponse verificationResponse = mailService.verifyMail(request);
-        if (!verificationResponse.isSuccess()) {
-            return ResponseEntity.badRequest().body(verificationResponse);
-        }
-
         // 비밀번호 밸리데이션
         if (!passwordValidator.test(request.getPassword())) {
-            MailVerifyResponse verificationResponse2 = new MailVerifyResponse(false, "PASSWORD_VALIDATION");
-            return ResponseEntity.badRequest().body(verificationResponse2);
+            MailVerifyResponse passwordValidateResponse = new MailVerifyResponse(false, "PASSWORD_VALIDATION");
+            return ResponseEntity.ok(passwordValidateResponse);
+        }
+
+        // 인증 실패 시
+        MailVerifyResponse verificationResponse = mailService.verifyMail(request);
+        if (!verificationResponse.isSuccess()) {
+            return ResponseEntity.ok(verificationResponse);
         }
 
         // 이메일 인증 성공 시 회원가입 시도
-        if ("VERIFIED".equals(verificationResponse.getError())) {
-            MailVerifyResponse signUpResponse = mailService.signUpUser(request);
-
-            if (signUpResponse.isSuccess()) {
-                // 회원가입 성공 시
-                return ResponseEntity.ok(signUpResponse);
-            } else {
-                // 회원가입 실패 시
-                return ResponseEntity.badRequest().body(signUpResponse);
-            }
-        } else {
-            // 기타 상황 처리
-            return ResponseEntity.badRequest().body(new MailVerifyResponse(false, "Invalid verification status."));
-        }
+        MailVerifyResponse signUpResponse = mailService.signUpUser(request);
+        return ResponseEntity.ok(signUpResponse);
     }
 }
