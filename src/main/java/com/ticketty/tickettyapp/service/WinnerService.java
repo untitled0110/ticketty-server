44423b6a44ad694ac671c7908c161bb4d4054296
Ji Yook <azure6.dev@gmail.com>
2024-03-11 21:35:10 +0900

    chore: 당첨자 가져오는 기능 - 시간 조건 수정

--- src/main/java/com/ticketty/tickettyapp/service/WinnerService.java
+++ src/main/java/com/ticketty/tickettyapp/service/WinnerService.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 
 import java.sql.Timestamp;
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -20,8 +21,17 @@ public class WinnerService {
 
     public WinnerAndPrizeResponse getTodayWinnerAndPrize() {
         // 오늘 날짜의 시작과 끝을 Timestamp 형식으로 변환
-        Timestamp startOfDay = Timestamp.valueOf(LocalDate.now().atStartOfDay());
-        Timestamp endOfDay = Timestamp.valueOf(LocalDate.now().plusDays(1).atStartOfDay().minusSeconds(1));
+        Timestamp startOfDay;
+        Timestamp endOfDay;
+
+        // 현재 시간이 오후 9시 이전인 경우
+        if (LocalTime.now().isBefore(LocalTime.of(21, 0))) {
+            startOfDay = Timestamp.valueOf(LocalDate.now().minusDays(1).atTime(LocalTime.of(21, 0))); // 어제 오후 9시
+            endOfDay = Timestamp.valueOf(LocalDate.now().atTime(LocalTime.of(20, 59, 59))); // 오늘 오후 8시 59분 59초
+        } else { // 현재 시간이 오후 9시 이후인 경우
+            startOfDay = Timestamp.valueOf(LocalDate.now().atTime(LocalTime.of(21, 0))); // 오늘 오후 9시
+            endOfDay = Timestamp.valueOf(LocalDate.now().plusDays(1).atTime(LocalTime.of(20, 59, 59))); // 내일 오후 8시 59분 59초
+        }
 
         // 오늘의 날짜로 저장된 당첨자 정보를 가져옴
         List<WinnerEntity> todayWinners = winnerEntityRepository.findByRegisteredAtBetween(startOfDay, endOfDay);
