04a711cca4394a171da1d1ba37e270b0d4857ee8
Ji Yook <azure6.dev@gmail.com>
2024-07-27 02:07:30 +0900

    feat: 당첨금 회수 (#40)

--- src/main/java/com/ticketty/tickettyapp/config/TicketDataGenerator.java
+++ src/main/java/com/ticketty/tickettyapp/config/TicketDataGenerator.java
@@ -1,35 +1,35 @@
-package com.ticketty.tickettyapp.config;
-import com.ticketty.tickettyapp.model.entity.TicketEntity;
-import com.ticketty.tickettyapp.model.entity.UserEntity;
-import com.ticketty.tickettyapp.repository.TicketEntityRepository;
-import com.ticketty.tickettyapp.repository.UserEntityRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
-
-import javax.annotation.PostConstruct;
-import javax.transaction.Transactional;
-import java.util.List;
-import java.util.Random;
-
-@Component
-@RequiredArgsConstructor
-public class TicketDataGenerator {
-
-    private final TicketEntityRepository ticketEntityRepository;
-    private final UserEntityRepository userEntityRepository;
-
-    @PostConstruct
-    @Transactional
-    public void generateTickets() {
-        List<UserEntity> users = userEntityRepository.findAll();
-        Random random = new Random();
-
-        for (int i = 0; i < 1000; i++) {
-            // 사용자 랜덤으로 선택
-            UserEntity user = users.get(random.nextInt(users.size()));
-
-            TicketEntity ticket = new TicketEntity(user);
-            ticketEntityRepository.save(ticket);
-        }
-    }
-}
+//package com.ticketty.tickettyapp.config;
+//import com.ticketty.tickettyapp.model.entity.TicketEntity;
+//import com.ticketty.tickettyapp.model.entity.UserEntity;
+//import com.ticketty.tickettyapp.repository.TicketEntityRepository;
+//import com.ticketty.tickettyapp.repository.UserEntityRepository;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Component;
+//
+//import javax.annotation.PostConstruct;
+//import javax.transaction.Transactional;
+//import java.util.List;
+//import java.util.Random;
+//
+//@Component
+//@RequiredArgsConstructor
+//public class TicketDataGenerator {
+//
+//    private final TicketEntityRepository ticketEntityRepository;
+//    private final UserEntityRepository userEntityRepository;
+//
+//    @PostConstruct
+//    @Transactional
+//    public void generateTickets() {
+//        List<UserEntity> users = userEntityRepository.findAll();
+//        Random random = new Random();
+//
+//        for (int i = 0; i < 1000; i++) {
+//            // 사용자 랜덤으로 선택
+//            UserEntity user = users.get(random.nextInt(users.size()));
+//
+//            TicketEntity ticket = new TicketEntity(user);
+//            ticketEntityRepository.save(ticket);
+//        }
+//    }
+//}
